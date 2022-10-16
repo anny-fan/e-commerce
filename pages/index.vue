f18dcf91550ba8dd219c71df71bdbb5cb04430b4
Anny Fan <annyfan.gmail.com>
2022-10-16 21:26:30 +0800

    add Footer, homeCarousel component

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <Nav/>
+    <Nav />
+    <HomeCarousel :sale_items="sale_items" />
+    <Footer />
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  async created() {
+    this.products = await this.$content("products").fetch();
+    this.sale_items = await this.$content("products")
+      .where({ onSale: true })
+      .fetch();
+  },
+  data() {
+    return {
+      products: null,
+      sale_items: null,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
